f1ee4b1beafd65e90cc4ccd2f35fba46dd6d7d6a
TaniguchiHiroto <55151347+hiroto302@users.noreply.github.com>
2024-03-13 19:08:12 +0900

    Initial Commit

---
+++ IOS_Camera_Sample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IOS_Camera_Sample
+//
+//  Created by hiroto taniguchi on 2024/03/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IOS_Camera_Sample/IOS_Camera_SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_Camera_SampleApp.swift
+//  IOS_Camera_Sample
+//
+//  Created by hiroto taniguchi on 2024/03/13.
+//
+
+import SwiftUI
+
+@main
+struct IOS_Camera_SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
