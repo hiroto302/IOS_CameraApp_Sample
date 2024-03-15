9831652e40d54c42939c90622320a238ef2cdeac
TaniguchiHiroto <55151347+hiroto302@users.noreply.github.com>
2024-03-15 18:12:18 +0900

    Update IOS_Camera_SampleApp.swift
    @main について

--- IOS_Camera_Sample/IOS_Camera_SampleApp.swift
+++ IOS_Camera_Sample/IOS_Camera_SampleApp.swift
@@ -7,9 +7,15 @@
 
 import SwiftUI
 
+// @main : この属性は、アプリケーションの開始点を示す
+// アプリケーションの実行時に、この属性が付与された構造体が自動的に呼び出される
 @main
+// Appプロトコルに準拠する構造体を定義し。
+// この構造体は、アプリケーションの設定とルートビューを提供する役割
 struct IOS_Camera_SampleApp: App {
     var body: some Scene {
+        // WindowGroup : アプリケーションのウィンドウを管理するための構造体
+        // 下記は、ContentViewをルートビューとして設定(アプリケーションが起動すると ContentViewが最初に表示さる)
         WindowGroup {
             ContentView()
         }
